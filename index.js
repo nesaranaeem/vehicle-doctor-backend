9492514ee6aa38107fd75127acec2a69befe7c85
nesaranaeem <hello@nesaran.com>
2022-11-03 18:32:41 +0600

    get by service id added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -17,12 +17,27 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
-client.connect((err) => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+const run = async () => {
+  try {
+    const database = client.db("vehicleDoctor");
+    const collectionName = database.collection("services");
+    app.get("/services", async (req, res) => {
+      const query = {};
+      const cursor = collectionName.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+    app.get("/service/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await collectionName.findOne(query);
+      res.send(service);
+    });
+  } finally {
+  }
+};
 
+run().catch((err) => console.error(err));
 app.get("/", (req, res) => {
   res.send("Server is running");
 });
